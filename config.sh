9e11e5d7d25f241c7d6f1a364b274ea58d03f33e
Genesio Valois <genas96@gmail.com>
2019-01-14 15:53:42 +0000

    Add gems

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+gem install overcommit rubocop flay fasterer foodcritic rails_best_practices sqlint brakeman
 # install plug - a minimalist Vim plugin manager.
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
